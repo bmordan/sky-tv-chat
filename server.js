562cf934eb9ae69f50bccd76c369ed08afbfbb24
bmordan <bernardmordan@gmail.com>
2019-03-16 00:20:52 +0000

    leveldb bugger to dockerise

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const sockets = require('express-ws')(app)
-const levelup = require('levelup')
-const leveldown = require('leveldown')
+const db = require('./redis-client')
 
 const clients = {}
-const db = levelup(leveldown('./db'))
 
 sockets.getWss().on('connection', (ws, req) => {
   const channel = req.url.replace(/.websocket/, '').replace(/\//g, '')
@@ -13,38 +11,17 @@ sockets.getWss().on('connection', (ws, req) => {
   ws.send(`${clients[channel].length} clients connected`)
 })
 
-db.on('put', (key, msg) => {
-  const [ channel, user, ts ] = key.split('|')
-  if (!clients[channel]) clients[channel] = []
-
-  clients[channel].forEach(client => {
-    client.send({ msg, user, ts })
-  })
-})
-
 app.get('/:channel', (req, res) => {
   const { channel } = req.params
-  const query = { gte: `${channel}!`, lte: `${channel}~` }
-  const channelMessages = db.createReadStream(query)
   const messages = []
-
-  channelMessages.on('data', ({ key, value }) => {
-    messages.push({
-      key: key.toString(),
-      msg: value.toString()
-    })
-  })
-
-  channelMessages.on('end', () => {
-    res.send(messages)
-  })
+  db
+  res.send(messages)
 })
 
 app.ws('/:channel/:user', (ws, req) => {
   ws.on('message', msg => {
     const { channel, user } = req.params
     const key = `${channel}|${user}|${new Date().getTime()}`
-    db.put(key, msg)
   })
 })
 
